5c0bbb897bc84ba25177f8b46e74db983ea1c153
Nihad96 <nihad.zatric@gmail.com>
2022-03-28 17:18:01 +0200

    ouain

--- footer.php
+++ footer.php
@@ -2,31 +2,23 @@
 
 <div id="footer">
 <div id="footer_text">
-    <div class="social" onclick="window.open(\'mailto:zatricnihad@gmail.com\');">
+    <div class="social" onclick="window.open('mailto:zatricnihad@gmail.com');">
         <img src="icons/gmail.png" alt="gmail" class="social_img">
         <p>Email</p>
     </div> 
     <div class="social not_pointer">
         <img src="icons/telephone.png" alt="telephone" class="social_img">
         <p>07.83.XX.XX.XX</p>
-    </div> 
-    <div class="social not_pointer">
-        <img src="icons/city.png" alt="city" class="social_img">
-        <p>Strasbourg</p>
-    </div> 
+    </div>
     
     
 </div>
 <div id="footer_social">
-    <div class="social" onclick="window.open(\'https://www.instagram.com/cirtazz/?hl=fr\');">
-        <img src="icons/instagram.png" alt="instagram" class="social_img">
-        <p>@cirtazz</p>
+    <div class="social not_pointer">
+        <img src="icons/city.png" alt="city" class="social_img">
+        <p>Strasbourg</p>
     </div>
-    <div class="social" onclick="window.open(\'https://twitter.com/Nihad_zt\');">
-        <img src="icons/twitter.png" alt="twitter" class="social_img">
-        <p>@Nihad_zt</p>
-    </div>
-    <div class="social" onclick="window.open(\'https://www.linkedin.com/in/nihadzatric/\');">
+    <div class="social" onclick="window.open('https://www.linkedin.com/in/nihadzatric/');">
         <img src="icons/linkedin.png" alt="linkedin" class="social_img">
         <p>@Nihad96</p>
     </div>
--- index.php
+++ index.php
@@ -11,14 +11,14 @@ include ('navbar.php');
             <img src="images/profil.jpg" alt="card1" class="card_img">
             <div class="card_content">
                 <p class="card_title">Qui suis-je ?</p>
-                <p class="card_text">Présentation de mon profil et des projets que j\'ai réalisé</p>
+                <p class="card_text">Présentation de mon profil et des projets que j'ai réalisé</p>
             </div>
         </div> 
         <div class="card" onclick="location='cedam.php'">
             <img src="images/cedam.jpg" alt="card1" class="card_img">
             <div class="card_content">
                 <p class="card_title">Cedam</p>
-                <p class="card_text">Présentation de Cedam, l\'entreprise dans laquelle je suis alternant</p>
+                <p class="card_text">Présentation de Cedam, l'entreprise dans laquelle je suis alternant</p>
             </div>
         </div> 
         
@@ -27,7 +27,7 @@ include ('navbar.php');
             <img src="images/travel.jpg" alt="card1" class="card_img">
             <div class="card_content">
                 <p class="card_title">E5 - Destinations</p>
-                <p class="card_text">Réalisation d\'un CRUD (create, read, update, delete) en AJAX, sur le thème des voyages</p>
+                <p class="card_text">Réalisation d'un CRUD (create, read, update, delete) en AJAX, sur le thème des voyages</p>
             </div>
         </div>
         
