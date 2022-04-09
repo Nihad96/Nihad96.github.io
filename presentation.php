0f05cb83d5ea8fe6cd9902366ab02894c7bf0803
Nihad <nihad.zatric@gmail.com>
2022-04-09 14:21:31 +0200

    fin

--- presentation.php
+++ presentation.php
@@ -1,1 +1,12 @@
 <?php
+
+$page = 'Qui suis-je ?';
+
+include ('navbar.php');
+?>
+
+    <div id="content">
+        <iframe style="margin:auto" src="https://docs.google.com/presentation/d/e/2PACX-1vSE_bPMUGpbdP43_XH5rU2AXRKRkWKAMIvl9tgy__7CC0riFGbJO2NFZeFBpzoUfCd4NOhxaZmcL4bk/embed?start=false&loop=false&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    </div>
+
+<?php include ('footer.php'); ?>
